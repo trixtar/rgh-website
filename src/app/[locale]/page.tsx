c8dab7a38de8103dc9d5d58ec9ff15de1f585cd3
Rita <magisterludi1789@gmail.com>
2026-04-21 18:06:17 +0200

    define colour palette

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -16,10 +16,8 @@ export default async function Home({ params }: { params: Promise<{ locale: strin
   setRequestLocale(locale);
   const t = await getTranslations('homepage');
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        {t('text')}
-      </main>
-    </div>
+    <main className="flex-1 w-full max-w-2xl mx-auto px-6 py-12"> {/* remove max-w mx- y px- py- */}
+      {t('text')}
+    </main>
   );
 }
