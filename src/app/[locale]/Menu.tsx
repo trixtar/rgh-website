af2807bf44c421841d7e2f545257b4772bcab915
Rita <magisterludi1789@gmail.com>
2026-04-28 18:35:34 +0200

    improve menu accessibility

--- src/app/[locale]/Menu.tsx
+++ src/app/[locale]/Menu.tsx
@@ -1,30 +1,78 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
-import { Link } from "@/i18n/navigation";
+import { Link, usePathname } from '@/i18n/navigation';
 
-export default function Menu({ isNavbar, isDropdown, onLinkClick }: { isNavbar?: boolean; isDropdown?: boolean; onLinkClick?: () => void; }) {
+export default function Menu({
+  isInNavbar,
+  isDropdown,
+  onLinkClick,
+}: {
+  isInNavbar?: boolean;
+  isDropdown?: boolean;
+  onLinkClick?: () => void;
+}) {
   const t = useTranslations('menu');
+  const pathname = usePathname();
 
-  let divStyle = 'flex flex-col text-center font-catchy font-bold text-4xl md:text-5xl';
-  let linkStyle = 'py-3 link-hover-style';
+  const menuItems = [
+    { href: 'bio', label: t('bio') },
+    { href: 'works', label: t('works') },
+    { href: 'performance', label: t('performance') },
+    { href: 'contact-details', label: t('contact') },
+  ] as const;
 
-  if (isNavbar) {
-    divStyle = 'hidden md:flex font-compact gap-6 text-2xl';
-    linkStyle = 'link-hover-style';
+  type MenuItem = (typeof menuItems)[number];
+  type Href = MenuItem['href'];
+
+  const isCurrent = (href: Href): boolean => pathname === `/${href}`;
+
+  let containerStyle =
+    'flex flex-col text-center font-catchy font-bold text-4xl md:text-5xl';
+  let linkStyle = 'py-3 basic-link-hover-style';
+  let nonCurrentStyle: string;
+  let currentStyle: string;
+  let focusStyle = 'basic-link-focus-style';
+  let activeStyle = 'bg-hope';
+
+  if (isInNavbar) {
+    containerStyle = 'hidden md:flex font-compact gap-6 text-2xl';
+    linkStyle = 'basic-link-hover-style';
+    currentStyle =
+      'underline decoration-midneutral decoration-dotted underline-offset-4';
   }
 
   if (isDropdown) {
-    divStyle = 'flex flex-col items-end z-99 font-compact text-2xl space-y-2';
-    linkStyle = 'pt-2 pb-3 px-4 shadow-md/30 rounded-xl bg-mystery text-lightneutral hover:underline hover:decoration-lightneutral hover:decoration-dotted hover:underline-offset-4 focus:bg-midneutral';
+    containerStyle =
+      'flex flex-col items-end z-99 font-compact text-2xl space-y-2 md:hidden';
+    linkStyle =
+      'pt-2 pb-3 px-4 shadow-md/30 rounded-xl text-lightneutral hover:underline hover:decoration-lightneutral hover:decoration-dotted hover:underline-offset-4 focus:bg-midneutral';
+    nonCurrentStyle = 'bg-mystery';
+    currentStyle = 'bg-midneutral';
+    focusStyle = 'focus:underline focus:decoration-lightneutral focus:decoration-dotted focus:underline-offset-4';
   }
 
+  const getLinkStyle = (href: Href): string => {
+    if (isCurrent(href)) {
+      return `${linkStyle} ${currentStyle}`;
+    }
+    return `${linkStyle} ${nonCurrentStyle}`;
+  };
+
   return (
-    <div className={divStyle}>
-      <Link onClick={onLinkClick} href='bio' className={linkStyle}>{t('bio')}</Link>
-      <Link onClick={onLinkClick} href='works' className={linkStyle}>{t('works')}</Link>
-      <Link onClick={onLinkClick} href='performance' className={linkStyle}>{t('performance')}</Link>
-      <Link onClick={onLinkClick} href='contact-details' className={linkStyle}>{t('contact')}</Link>
-    </div>
+    <ul className={containerStyle}>
+      {menuItems.map((item) => (
+        <li key={item.href} className={getLinkStyle(item.href)}>
+          <Link
+            className={focusStyle}
+            onClick={onLinkClick}
+            href={item.href}
+            aria-current={isCurrent(item.href) ? 'page' : undefined}
+          >
+            {item.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 }
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,31 +1,65 @@
 'use client';
 
-import Menu from "@/app/[locale]/Menu";
-import { Link, usePathname } from "@/i18n/navigation";
-import { useState } from "react";
+import Menu from '@/app/[locale]/Menu';
+import { useTranslations } from 'next-intl';
+import { Link, usePathname } from '@/i18n/navigation';
+import { useState } from 'react';
 
-const NavMenu = ({ onClick, isDropdownShown }: { onClick: () => void; isDropdownShown?: boolean; }) => {
+const NavMenu = ({
+  onClick,
+  isDropdownShown,
+}: {
+  onClick: () => void;
+  isDropdownShown?: boolean;
+}) => {
+  const t = useTranslations('menu');
+
   return (
-    <nav className='flex relative items-center'>
-      <button className='md:hidden border-transparent border-3 hover:cursor-pointer hover:border-mystery hover:border-dotted' onClick={onClick} aria-label='Toggle menu' aria-expanded={isDropdownShown ? true : false}>
+    <nav className='flex relative items-center' aria-label={t('label')}>
+      <button
+        className='md:hidden border-transparent border-3 hover:cursor-pointer hover:border-mystery hover:border-dotted'
+        onClick={onClick}
+        aria-label={isDropdownShown ? t('close') : t('open')}
+        aria-expanded={isDropdownShown ? true : false}
+      >
         {isDropdownShown ? (
-          <svg id='close-icon' className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24'>
-          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12' />
-        </svg>
+          <svg
+            className='w-6 h-6'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M6 18L18 6M6 6l12 12'
+            />
+          </svg>
         ) : (
-          <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24'>
-            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16M4 18h16' />
+          <svg
+            className='w-6 h-6'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M4 6h16M4 12h16M4 18h16'
+            />
           </svg>
         )}
       </button>
-      <Menu isNavbar />
+      <Menu isInNavbar />
       {isDropdownShown && (
         <div className='absolute top-[46px] right-0'>
           <Menu isDropdown onLinkClick={onClick} />
         </div>
       )}
     </nav>
-  )
+  );
 };
 
 export default function Navbar() {
@@ -36,11 +70,25 @@ export default function Navbar() {
   };
 
   const pathname = usePathname();
+  const title = 'Rita Gonzalez Hesaynes';
   const isHomepage = pathname === '/';
+
   return (
     <header className='p-4 flex justify-between items-end'>
-      <Link onClick={() => setDropdownShown(false)} href='/' className='font-semibold text-3xl font-compact link-hover-style'>Rita Gonzalez Hesaynes</Link>
-      {!isHomepage && <NavMenu onClick={handleClick} isDropdownShown={isDropdownShown} />}
+      <h1
+        className={`font-semibold text-3xl font-compact ${isHomepage ? '' : 'basic-link-hover-style'}`}
+      >
+        {isHomepage ? (
+          <span>{title}</span>
+        ) : (
+          <Link className='focus-hover-style' onClick={() => setDropdownShown(false)} href='/'>
+            {title}
+          </Link>
+        )}
+      </h1>
+      {!isHomepage && (
+        <NavMenu onClick={handleClick} isDropdownShown={isDropdownShown} />
+      )}
     </header>
   );
 }
